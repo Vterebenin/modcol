4a000b16caa7c51a8326fbab677796ed054cc0b1
ValTerebenin <v.terebenin.@andersenlab.com>
2022-09-20 14:12:07 +0300

    FEAT: new build config added

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
+import {resolve} from 'path';
 import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
-
-const BASE = {
-    external: ['lodash'],
-    plugins: [],
-    input: 'src/index.js',
-};
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -23,6 +18,17 @@ export default defineConfig({
         globals: true,
     },
     build: {
-        rollupOptions: BASE,
+        lib: {
+            entry: resolve(__dirname, 'src/index.js'),
+            name: 'modcol',
+            // the proper extensions will be added
+            fileName: 'modcol',
+        },
+        rollupOptions: {
+            // make sure to externalize deps that shouldn't be bundled
+            // into your library
+            input: 'src/index.js',
+            external: ['lodash'],
+        },
     },
 });
